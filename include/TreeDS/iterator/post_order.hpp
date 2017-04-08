08d9d7fb7e858317641df55f82131ec469f91e8d
barsan-ds <barsan.md@gmail.com>
2017-04-08 14:26:31 +0200

    post_order iterator

---
+++ include/TreeDS/iterator/post_order.hpp
@@ -0,0 +1,53 @@
+#ifndef INCLUDE_TREEDS_ITERATOR_POST_ORDER_HPP_
+#define INCLUDE_TREEDS_ITERATOR_POST_ORDER_HPP_
+
+#include "helper.hpp"
+
+namespace ds {
+
+template<typename T>
+class node;
+
+class post_order final {
+
+public:
+	constexpr post_order() = default;
+	~post_order() = default;
+
+	template<typename T>
+	const node<T>* increment(const node<T>& n) const {
+		auto prev = &n;
+		auto next = n.parent();
+		if (!next || prev == next->last_child()) {
+			return next; // found
+		}
+		return descent(*next->right_child(), [](const node<T>& n) {
+			return n.first_child();
+		});
+	}
+
+	template<typename T>
+	const node<T>* decrement(const node<T>& n) const {
+		auto result = n.last_child();
+		if (result) {
+			return result;
+		}
+		return cross_bridge_left(n);
+	}
+
+	template<typename T> const node<T>* go_first(const node<T>& root) const {
+		return descent(root, [](const node<T>& n){
+			return n.first_child();
+		});
+	}
+
+	template<typename T>
+	const node<T>* go_last(const node<T>& root) const {
+		return &root;
+	}
+
+};
+
+} /* namespace ds */
+
+#endif /* INCLUDE_TREEDS_ITERATOR_POST_ORDER_HPP_ */
