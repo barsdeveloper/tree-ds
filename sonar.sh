0dd179097afc0e8476423991907f52eedb37686f
barsan-ds <barsan.md@gmail.com>
2018-09-16 18:22:38 +0200

    Sonar integration

---
+++ sonar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export PLATFORM=x86_64
+build-wrapper-linux-x86-64 --out-dir bw_output make clean all
+sonar-scanner \
+  -Dsonar.projectKey=tree-ds \
+  -Dsonar.organization=barsan-ds-github \
+  -Dsonar.projectVersion=1.0 \
+  -Dsonar.sources=./include \
+  -Dsonar.cfamily.build-wrapper-output=bw_output \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=e28169718a4419456d1da1452392b20efbe40c6b \
+  -Dsonar.coverageReportPaths=./build/coverage/kcov-merged/sonarqube.xml
