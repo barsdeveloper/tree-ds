a6917ce53740dfe48cb67db182ab930a1d5a2236
barsan-ds <barsan.md@gmail.com>
2018-10-11 21:17:11 +0200

    nary tree tests added

--- test/NaryTreeIterationTest.cpp
+++ test/NaryTreeIterationTest.cpp
@@ -3,16 +3,15 @@
 #include <iterator>
 #include <list>
 
-#include <TreeDS/binary_tree>
 #include <TreeDS/nary_tree>
 
 using namespace std;
 using namespace ds;
 
 Q_DECLARE_METATYPE(list<char>);
-Q_DECLARE_METATYPE(binary_tree<char>);
+Q_DECLARE_METATYPE(nary_tree<char>);
 
-class TreeIterationTest : public QObject {
+class NaryTreeIterationTest : public QObject {
 
     Q_OBJECT
 
@@ -21,65 +20,34 @@ class TreeIterationTest : public QObject {
     void iteration_data();
 };
 
-void TreeIterationTest::iteration() {
-    QFETCH(binary_tree<char>, binary);
+void NaryTreeIterationTest::iteration() {
+    QFETCH(nary_tree<char>, t);
     QFETCH(int, expectedSize);
     QFETCH(list<char>, expectedPreOrder);
-    QFETCH(list<char>, expectedInOrder);
     QFETCH(list<char>, expectedPostOrder);
     QFETCH(list<char>, expectedBreadthFirst);
-    nary_tree<char> nary(binary);
-    int actualSize = static_cast<int>(binary.size());
+    int actualSize = static_cast<int>(t.size());
 
     // Preliminary test
-    QCOMPARE(nary, binary);
     QCOMPARE(actualSize, expectedSize);
 
     /*   ---   Forward order test   ---   */
-    // Binary
-    list<char> actualBinaryPreOrder;
-    list<char> actualBinaryInOrder;
-    list<char> actualBinaryPostOrder;
-    list<char> actualBinaryBreadthFirst;
-    // Nary
-    list<char> actualNaryPreOrder;
-    list<char> actualNaryInOrder;
-    list<char> actualNaryPostOrder;
-    list<char> actualNaryBreadthFirst;
+    list<char> actualPreOrder;
+    list<char> actualPostOrder;
+    list<char> actualBreadthFirst;
 
-    // Traverse binary
-    copy(binary.begin<pre_order>(), binary.end<pre_order>(), back_inserter(actualBinaryPreOrder));
-    copy(binary.begin<in_order>(), binary.end<in_order>(), back_inserter(actualBinaryInOrder));
-    copy(binary.begin<post_order>(), binary.end<post_order>(), back_inserter(actualBinaryPostOrder));
+    copy(t.begin<pre_order>(), t.end<pre_order>(), back_inserter(actualPreOrder));
+    copy(t.begin<post_order>(), t.end<post_order>(), back_inserter(actualPostOrder));
     copy(
-        binary.begin<breadth_first<binary_node<char>>>(),
-        binary.end<breadth_first<binary_node<char>>>(),
-        back_inserter(actualBinaryBreadthFirst));
-    // Traverse nary
-    copy(nary.begin<pre_order>(), nary.end<pre_order>(), back_inserter(actualNaryPreOrder));
-    copy(nary.begin<post_order>(), nary.end<post_order>(), back_inserter(actualNaryPostOrder));
-    copy(
-        nary.begin<breadth_first<nary_node<char>>>(),
-        nary.end<breadth_first<nary_node<char>>>(),
-        back_inserter(actualNaryBreadthFirst));
+        t.begin<breadth_first<nary_node<char>>>(),
+        t.end<breadth_first<nary_node<char>>>(),
+        back_inserter(actualBreadthFirst));
 
-    // Compare results binary
-    QCOMPARE(actualBinaryPreOrder, expectedPreOrder);
-    QCOMPARE(actualBinaryInOrder, expectedInOrder);
-    QCOMPARE(actualBinaryPostOrder, expectedPostOrder);
-    QCOMPARE(actualBinaryBreadthFirst, expectedBreadthFirst);
-    // Compare results nary
-    QCOMPARE(actualNaryPreOrder, expectedPreOrder);
-    QCOMPARE(actualNaryPostOrder, expectedPostOrder);
-    QCOMPARE(actualNaryBreadthFirst, expectedBreadthFirst);
+    QCOMPARE(actualPreOrder, expectedPreOrder);
+    QCOMPARE(actualPostOrder, expectedPostOrder);
+    QCOMPARE(actualBreadthFirst, expectedBreadthFirst);
 
     /*   ---   Backward order test   ---   */
-    // Binary
-    list<char> actualBinaryReversePreOrder;
-    list<char> actualBinaryReverseInOrder;
-    list<char> actualBinaryReversePostOrder;
-    list<char> actualBinaryReverseBreadthFirst;
-    // Nary
     list<char> actualNaryReversePreOrder;
     list<char> actualNaryReversePostOrder;
     list<char> actualNaryReverseBreadthFirst;
@@ -89,53 +57,33 @@ void TreeIterationTest::iteration() {
     list<char> expectedReversePostOrder;
     list<char> expectedReverseBreadthFirst;
 
-    // Reverse traverse binary
-    copy(binary.rbegin<pre_order>(), binary.rend<pre_order>(), back_inserter(actualBinaryReversePreOrder));
-    copy(binary.rbegin<in_order>(), binary.rend<in_order>(), back_inserter(actualBinaryReverseInOrder));
-    copy(binary.rbegin<post_order>(), binary.rend<post_order>(), back_inserter(actualBinaryReversePostOrder));
+    copy(t.rbegin<pre_order>(), t.rend<pre_order>(), back_inserter(actualNaryReversePreOrder));
+    copy(t.rbegin<post_order>(), t.rend<post_order>(), back_inserter(actualNaryReversePostOrder));
     copy(
-        binary.rbegin<breadth_first<binary_node<char>>>(),
-        binary.rend<breadth_first<binary_node<char>>>(),
-        back_inserter(actualBinaryReverseBreadthFirst));
-
-    // Reverse traverse nary
-    copy(nary.rbegin<pre_order>(), nary.rend<pre_order>(), back_inserter(actualNaryReversePreOrder));
-    copy(nary.rbegin<post_order>(), nary.rend<post_order>(), back_inserter(actualNaryReversePostOrder));
-    copy(
-        nary.rbegin<breadth_first<nary_node<char>>>(),
-        nary.rend<breadth_first<nary_node<char>>>(),
+        t.rbegin<breadth_first<nary_node<char>>>(),
+        t.rend<breadth_first<nary_node<char>>>(),
         back_inserter(actualNaryReverseBreadthFirst));
 
-    // Reverse expected
     copy(expectedPreOrder.rbegin(), expectedPreOrder.rend(), back_inserter(expectedReversePreOrder));
-    copy(expectedInOrder.rbegin(), expectedInOrder.rend(), back_inserter(expectedReverseInOrder));
     copy(expectedPostOrder.rbegin(), expectedPostOrder.rend(), back_inserter(expectedReversePostOrder));
     copy(expectedBreadthFirst.rbegin(), expectedBreadthFirst.rend(), back_inserter(expectedReverseBreadthFirst));
 
-    // Compare results binary
-    QCOMPARE(actualBinaryReversePreOrder, expectedReversePreOrder);
-    QCOMPARE(actualBinaryReverseInOrder, expectedReverseInOrder);
-    QCOMPARE(actualBinaryReversePostOrder, expectedReversePostOrder);
-    QCOMPARE(actualBinaryReverseBreadthFirst, expectedReverseBreadthFirst);
-    // Compare results nary
     QCOMPARE(actualNaryReversePreOrder, expectedReversePreOrder);
     QCOMPARE(actualNaryReversePostOrder, expectedReversePostOrder);
     QCOMPARE(actualNaryReverseBreadthFirst, expectedReverseBreadthFirst);
 }
 
-void TreeIterationTest::iteration_data() {
-    QTest::addColumn<binary_tree<char>>("binary");
+void NaryTreeIterationTest::iteration_data() {
+    QTest::addColumn<nary_tree<char>>("t");
     QTest::addColumn<int>("expectedSize");
     QTest::addColumn<list<char>>("expectedPreOrder");
-    QTest::addColumn<list<char>>("expectedInOrder");
     QTest::addColumn<list<char>>("expectedPostOrder");
     QTest::addColumn<list<char>>("expectedBreadthFirst");
 
     /******************************************************************************************************************/
     QTest::newRow("Single character")
-        << binary_tree<char>(n('#'))
+        << nary_tree<char>(n('#'))
         << 1
-        << list<char>{'#'}
         << list<char>{'#'}
         << list<char>{'#'}
         << list<char>{'#'};
@@ -143,26 +91,23 @@ void TreeIterationTest::iteration_data() {
     /******************************************************************************************************************/
     QTest::newRow("Root with a left child")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('1')(
                    n('2')))
 
         << 2
         << list<char>{'1', '2'}
-        << list<char>{'2', '1'}
         << list<char>{'2', '1'}
         << list<char>{'1', '2'};
 
     /******************************************************************************************************************/
     QTest::newRow("Root with a right child")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('1')(
-                   n(),
                    n('2')))
 
         << 2
-        << list<char>{'1', '2'}
         << list<char>{'1', '2'}
         << list<char>{'2', '1'}
         << list<char>{'1', '2'};
@@ -170,13 +115,11 @@ void TreeIterationTest::iteration_data() {
     /******************************************************************************************************************/
     QTest::newRow("Small tree")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('a')(
                    n('b')(
                        n('d')(
-                           n('h'),
-                           n() // This can be omitted but I want to test it
-                           ),
+                           n('h')),
                        n('e')),
                    n('c')(
                        n('f')(
@@ -186,14 +129,13 @@ void TreeIterationTest::iteration_data() {
 
         << 10
         << list<char>{'a', 'b', 'd', 'h', 'e', 'c', 'f', 'j', 'k', 'g'}
-        << list<char>{'h', 'd', 'b', 'e', 'a', 'j', 'f', 'k', 'c', 'g'}
         << list<char>{'h', 'd', 'e', 'b', 'j', 'k', 'f', 'g', 'c', 'a'}
         << list<char>{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k'};
 
     /******************************************************************************************************************/
     QTest::newRow("Big tree")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('a')(
                    n('b')(
                        n('c')(
@@ -202,40 +144,29 @@ void TreeIterationTest::iteration_data() {
                                    n('f'),
                                    n('g')),
                                n('h')(
-                                   n(),
                                    n('o'))),
                            n('i')(
-                               n(),
                                n('n')(
-                                   n(),
                                    n('p')))),
                        n('j')(
-                           n(),
                            n('m')(
-                               n(),
                                n('q')(
-                                   n(),
                                    n('t'))))),
                    n('k')(
-                       n(),
                        n('l')(
-                           n(),
                            n('r')(
-                               n(),
                                n('s')(
-                                   n(),
                                    n('u')))))))
 
         << 21
         << list<char>{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'o', 'i', 'n', 'p', 'j', 'm', 'q', 't', 'k', 'l', 'r', 's', 'u'}
-        << list<char>{'f', 'e', 'g', 'd', 'h', 'o', 'c', 'i', 'n', 'p', 'b', 'j', 'm', 'q', 't', 'a', 'k', 'l', 'r', 's', 'u'}
         << list<char>{'f', 'g', 'e', 'o', 'h', 'd', 'p', 'n', 'i', 'c', 't', 'q', 'm', 'j', 'b', 'u', 's', 'r', 'l', 'k', 'a'}
         << list<char>{'a', 'b', 'k', 'c', 'j', 'l', 'd', 'i', 'm', 'r', 'e', 'h', 'n', 'q', 's', 'f', 'g', 'o', 'p', 't', 'u'};
 
     /******************************************************************************************************************/
     QTest::newRow("All left child")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('1')(
                    n('2')(
                        n('3')(
@@ -254,46 +185,30 @@ void TreeIterationTest::iteration_data() {
 
         << 15
         << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
-        << list<char>{'F', 'E', 'D', 'C', 'B', 'A', '9', '8', '7', '6', '5', '4', '3', '2', '1'}
         << list<char>{'F', 'E', 'D', 'C', 'B', 'A', '9', '8', '7', '6', '5', '4', '3', '2', '1'}
         << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     /******************************************************************************************************************/
     QTest::newRow("All right child")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('1')(
-                   n(),
                    n('2')(
-                       n(),
                        n('3')(
-                           n(),
                            n('4')(
-                               n(),
                                n('5')(
-                                   n(),
                                    n('6')(
-                                       n(),
                                        n('7')(
-                                           n(),
                                            n('8')(
-                                               n(),
                                                n('9')(
-                                                   n(),
                                                    n('A')(
-                                                       n(),
                                                        n('B')(
-                                                           n(),
                                                            n('C')(
-                                                               n(),
                                                                n('D')(
-                                                                   n(),
                                                                    n('E')(
-                                                                       n(),
                                                                        n('F'))))))))))))))))
 
         << 15
-        << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
         << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
         << list<char>{'F', 'E', 'D', 'C', 'B', 'A', '9', '8', '7', '6', '5', '4', '3', '2', '1'}
         << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
@@ -301,36 +216,28 @@ void TreeIterationTest::iteration_data() {
     /******************************************************************************************************************/
     QTest::newRow("ZigZag")
 
-        << binary_tree<char>(
+        << nary_tree<char>(
                n('1')(
                    n('2')(
-                       n(),
                        n('3')(
                            n('4')(
-                               n(),
                                n('5')(
                                    n('6')(
-                                       n(),
                                        n('7')(
                                            n('8')(
-                                               n(),
                                                n('9')(
                                                    n('A')(
-                                                       n(),
                                                        n('B')(
                                                            n('C')(
-                                                               n(),
                                                                n('D')(
                                                                    n('E')(
-                                                                       n(),
                                                                        n('F'))))))))))))))))
 
         << 15
         << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
-        << list<char>{'2', '4', '6', '8', 'A', 'C', 'E', 'F', 'D', 'B', '9', '7', '5', '3', '1'}
         << list<char>{'F', 'E', 'D', 'C', 'B', 'A', '9', '8', '7', '6', '5', '4', '3', '2', '1'}
         << list<char>{'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 }
 
-QTEST_MAIN(TreeIterationTest);
-#include "TreeIterationTest.moc"
+QTEST_MAIN(NaryTreeIterationTest);
+#include "NaryTreeIterationTest.moc"
