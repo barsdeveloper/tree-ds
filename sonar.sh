620d17bc21272f821379aaef3962b8e5496ef242
barsan-md <barsan.md@gmail.com>
2019-01-17 22:53:55 +0100

    SonarQube key not public anymore

--- sonar.sh
+++ sonar.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-export PLATFORM=x86_64
-build-wrapper-linux-x86-64 --out-dir ./build/bw_output make clean all
 sonar-scanner \
-  -Dsonar.projectKey=tree-ds \
-  -Dsonar.organization=barsan-ds-github \
+  -Dsonar.projectKey=barsan-md_tree-ds \
+  -Dsonar.organization=barsan-md-github \
   -Dsonar.projectVersion=1.0 \
   -Dsonar.sources=./include \
-  -Dsonar.cfamily.build-wrapper-output=./build/bw_output \
+  -Dsonar.cfamily.build-wrapper-output=bw-output \
   -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=e28169718a4419456d1da1452392b20efbe40c6b \
+  -Dsonar.login=$(cat ./sonar_key) \
   -Dsonar.coverageReportPaths=./build/coverage/kcov-merged/sonarqube.xml
+
